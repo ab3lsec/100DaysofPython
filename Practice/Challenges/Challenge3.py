7d5ecf354a889c58377f9d16bd6a0e456be97f55
Abel <abelvarghese077@gmail.com>
2024-04-23 22:31:58 -0400

    Added Notes

---
+++ Practice/Challenges/Challenge3.py
@@ -0,0 +1,20 @@
+# Challenge 3:
+# Write a Python script that prompts the user to enter their age. 
+# Based on their age, print out the appropriate message:
+
+# - If the age is less than 18, print "You are a minor."
+# - If the age is between 18 and 65 (inclusive), print "You are an adult."
+# - If the age is greater than 65, print "You are a senior citizen."
+
+age = int(input("Enter Your Age: "))
+
+if (age >= 0 and age < 18):
+    print("You are a minor.")
+
+elif (age >= 18 and age <= 65):
+    print("You are an adult.")
+
+else:
+    print("You are a senior citizen.")
+
+    
---
+++ Practice/Challenges/Challenge4.py
@@ -0,0 +1,29 @@
+# Challenge 7:
+# Write a Python script that prompts the user to enter a day of the week (e.g., "Monday", "Tuesday", etc.). 
+# Use the `match` statement to check the input and print out a message based on the day entered:
+
+# - If the input matches "Monday" or "Tuesday", print "It's the start of the week."
+# - If the input matches "Wednesday" or "Thursday", print "It's midweek."
+# - If the input matches "Friday", print "It's almost the weekend."
+# - If the input matches "Saturday" or "Sunday", print "It's the weekend."
+# - If the input doesn't match any of the above, print "Invalid day entered."
+
+day = input("Enter a day: ")
+day = day.lower()
+
+match day:
+
+    case "monday" | "tuesday":
+        print("It's the start of the week.")
+
+    case "wednesday" | "thursday":
+        print("It's midweek.")
+
+    case "friday":
+        print("It's almost the weekend.")
+
+    case "saturday" | "sunday":
+        print("It's the weekend.")
+
+    case _:
+        print("Invalid day entered.")
