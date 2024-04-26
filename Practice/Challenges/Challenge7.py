cd8c2a4dd02fe2860463d9414df727e88495b33a
Abel <abelvarghese077@gmail.com>
2024-04-25 23:55:38 -0400

    Added Notes

---
+++ Practice/Challenges/Challenge7.py
@@ -0,0 +1,16 @@
+# Challenge 7: 
+# Write a Python function called `calcRectArea` that takes two arguments: `length` and `width`.
+# The function should calculate and return the area of the rectangle.
+
+# Next, prompt the user to enter the length and width of a rectangle, and call the `calcRectArea` function with the user-provided values. 
+# Finally, print out the calculated area.
+
+def calcRectArea(length, width):
+    return length * width
+
+len = int(input("Enter the Length: "))
+wid = int(input("Enter the Width: "))
+
+area = calcRectArea(len, wid)
+
+print("The Area of Rectangle is: ", area)
---
+++ Practice/Challenges/Challenge8.py
@@ -0,0 +1,26 @@
+# Challenge 8:
+# Imagine you're analyzing the security of a web application as part of a bug bounty program. 
+# Write a function called `checkPasswStrength()` that takes a password as input and returns a message indicating its strength:
+
+# - If the password length is less than 8 characters, return "Weak password: Password length should be at least 8 characters."
+# - If the password contains only letters or only digits, return "Weak password: Password should contain a mix of letters and digits."
+# - If the password contains both letters and digits and has a length of at least 8 characters, return "Strong password: Password is strong."
+
+# Next, prompt the user to enter a password, call the `checkPasswStrength()` with the user-provided password. 
+# Finally, print out the message indicating the password strength.
+
+def checkPasswStrength(passw):
+    if len(passw) < 8:
+        return "Weak password: Password length should be at least 8 characters."
+    
+    elif passw.isalpha() or passw.isnumeric():
+        return "Weak password: Password should contain a mix of letters and digits."
+    
+    else:
+        return "Strong password: Password is strong."
+    
+    
+password = input("Enter the Password: ")
+
+result = checkPasswStrength(password)
+print(result)
