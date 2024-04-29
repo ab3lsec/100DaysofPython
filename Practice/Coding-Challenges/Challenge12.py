89f6b35894986e1f5953ff0a5c6a09ce605eb3cd
Abel <abelvarghese077@gmail.com>
2024-04-28 23:08:13 -0400

    Added Challenges

--- Practice/Coding-Challenges/Challenge12.py
+++ Practice/Coding-Challenges/Challenge12.py
@@ -1,7 +1,7 @@
-# Challenge 16: URL Path Analysis
+# Challenge 12: URL Path Analysis
 
 # In bug bounty hunting, understanding the structure and potential vulnerabilities of URL paths is crucial. 
-# Write a Python function called analyze_url_paths that takes a list of URLs as input and analyzes the paths of those URLs for common security vulnerabilities.
+# Write a Python function called analyzeURL() that takes a list of URLs as input and analyzes the paths of those URLs for common security vulnerabilities.
 
 # The function should perform the following analysis:
 
@@ -11,13 +11,27 @@
 
 # Next, prompt the user to enter URLs. Ask the user to enter the URLs separated by commas (e.g., "https://example.com/,https://example.com/admin"). 
 # Split the input string by commas and convert each part into a list of URLs. 
-# Finally, call the analyze_url_paths function with the user-provided list of URLs and print out the analysis result.
+# Finally, call the analyzeURL() function with the user-provided list of URLs and print out the analysis result.
 
 # This challenge will help you practice analyzing URLs for potential security vulnerabilities, which is a critical skill in bug bounty hunting.
 
 
-# Using the split function to take the input. 
+
 urls = input("Enter the URLs: ").split(",")
 
-print(urls)
+print("Scanning for Vulnerabilities....")
 
+sensitiveWords = ["admin", "config", "password"]
+
+def analyzeURL(urls):
+
+    for url in urls:
+        if url.count("../") > 0 or url.count("%2e%2e%2f" > 0):
+            print("Vulnerabilty found in: ", url)
+
+        for word in sensitiveWords:
+            if word in url:
+                print("Vulnerabilty found in: ", url)
+
+
+analyzeURL(urls)
--- Practice/Coding-Challenges/Challenge13.py
+++ Practice/Coding-Challenges/Challenge13.py
@@ -1,9 +1,10 @@
 # Challenge 16: URL Sanitization
 
-# One common task in bug bounty hunting is to perform URL sanitization to prevent various types of attacks, such as Cross-Site Scripting (XSS) and SQL Injection.
+# One common task in bug bounty hunting is to perform URL sanitization 
+# to prevent various types of attacks, such as Cross-Site Scripting (XSS) and SQL Injection.
 
-# Write a Python function called sanitize_url that takes a URL as input 
-# and returns a sanitized version of the URL. The function should perform the following sanitization steps:
+# Write a Python function called sanitizeURL() that takes a URL as input and returns a sanitized version of the URL.
+#  The function should perform the following sanitization steps:
 
 # - Remove any trailing whitespace from the URL.
 # - Convert the URL to lowercase.
@@ -14,3 +15,21 @@
 # Next, prompt the user to enter a URL. Call the sanitize_url function with the user-provided URL and print out the sanitized URL.
 
 # This challenge will help you practice string manipulation techniques commonly used in bug bounty hunting to mitigate security vulnerabilities.
+
+
+def sanitizeURL(url):
+
+    print("Sanitizing Your URL.....")
+    url = url.lower()
+    url = url.strip()
+    url = url.replace("javascript:", "")
+    url = url.rstrip("/")
+    url = url.lstrip("/")
+
+    return url
+
+
+# url = "/javascript:http://EXAMPLE.com/       "
+
+url = input("Enter the URL: ")
+print("Here is the Sanitized version of your URL:", sanitizeURL(url))
